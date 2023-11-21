b5501c82b6ddcb5ef372ef85041ecc47d04012f2
julieta hernandez <hernandezjulieta2105@gmail.com>
2023-11-21 18:18:26 -0300

    Update Tp4.py

--- TP4/Tp4.py
+++ TP4/Tp4.py
@@ -1,11 +1,7 @@
 import sys
 import random
 import math as m
-
-
-def imprimirMatriz(matriz):
-    for fila in matriz:
-        print(fila)
+import copy
 
 
 def calc_probF_Salida(probF_E, mat_canal):
@@ -30,11 +26,11 @@ def calc_entropia_Apriori(probF_E, probF_S):
 
     for p in probF_E:
         if p != 0:
-            entropia_A += p * math.log(1 / p)
+            entropia_A += p * m.log(1 / p)
 
     for p in probF_S:
         if p != 0:
-            entropia_B += p * math.log(1 / p)
+            entropia_B += p * m.log(1 / p)
 
     return entropia_A, entropia_B
 
@@ -60,47 +56,51 @@ def cal_prob_suceso_simul(prob_condicionales, probF_S):
 
 
 def calc_equivocacion(prob_suceso_simul, prob_condicionales):
+    Hab = 0
     for i in range(2):
         for j in range(2):
             if prob_condicionales[i][j] != 0:
                 Hab += prob_suceso_simul[i][j] * m.log(1 / prob_condicionales[i][j])
-    return equi
+    return Hab
 
 
 def generaMensajes(N, M, probF_E):
     msn = [[0 for _ in range(len(M))] for _ in range(len(N))]
-
     for i in range(len(N)):
         for j in range(len(M)):
             msn[i][j] = 0 if random.random() <= probF_E[0] else 1
-
     return msn
 
 
 def metodoParidadCruzada(mensajes, N, M):
     paridades = copy.deepcopy(mensajes)
-
-    for i in N:
-        cant = sum(paridades[i][j] for j in M)
+    N = int(N)
+    M = int(M)
+    for i in range(N):
+        cant = 0
+        for j in range(M):
+            cant += paridades[i][j]
         if cant % 2 == 0:
-            paridades[i][M] = 0
+            paridades[i].append(0)
         else:
-            paridades[i][M] = 1
+            paridades[i].append(1)
 
-    for j in M:
-        cant = sum(paridades[i][j] for i in N)
+    for j in range(M):
+        cant = sum(paridades[i][j] for i in range(N))
         if cant % 2 == 0:
-            paridades[N][j] = 0
+            paridades.append([0])
         else:
-            paridades[N][j] = 1
+            paridades.append([1])
 
     return paridades
 
 
 def enviaMensajes(matriz, mat_canal):
+    filas = len(matriz)
+    columnas = len(matriz[0]) if filas > 0 else 0
     msn = []
-    for i in N:
-        for j in M:
+    for i in range(filas):
+        for j in range(columnas):
             x = random.random()
             msn[i][j] = 0 if random.random() <= mat_canal[matriz[i][j]][0] else 1
     return msn
@@ -110,7 +110,6 @@ def main():
     args = sys.argv
 
     # tpi4 probs.txt N M [-p]
-    print("len(Args)", len(args))
     if len(args) > 7 or len(args) < 4:
         print("Uso: tpi4 probs.txt N M [-p]")
         sys.exit(1)
@@ -119,9 +118,9 @@ def main():
     N = args[3]
     M = args[4]
     action = args[5]
-    print("action", action)
-    mat_canal = [[0, 0], [0, 0]]
-    # P(ai)
+
+    mat_canal = []
+
     probF_E = [0, 0]
 
     #!a chequear
@@ -129,8 +128,8 @@ def main():
         for i, linea in enumerate(f):
             valores = list(map(float, linea.split()))
             if i == 0:
-                probF = valores
-            elif i == 1 or i == 2:
+                probF_E = valores
+            elif 1 <= i <= 2:
                 mat_canal.append(valores)
 
     # P(bi)
@@ -144,7 +143,7 @@ def main():
 
     # H(A/b=0) , H(A/b=1)  #! tmb con B?
     entropia_Aposteriori_0, entropia_Aposteriori_1 = calc_entropia_Aposteriori(
-        probF, mat_canal, prob_condicionales
+        probF_E, mat_canal, prob_condicionales
     )
 
     # P(ai,bi)
@@ -154,15 +153,24 @@ def main():
     equivocacion = calc_equivocacion(prob_suceso_simul, prob_condicionales)
 
     # I(A,B) = I (B,A)
-    info_mutua = entropia_Apriori - equivocacion
+    info_mutua = entropia_Apriori_A - equivocacion
 
-    print("MATRIZ DEL CANAL:", imprimirMatriz(mat_canal))
-    print("PROBABILIDAD DE LA FUENTE DE ENTRADA P(ai):", probFE)
-    print("PROBABILIDAD DE LA FUENTE DE SALIDA P(bi):", probF_S)
-    print("PROBABILIDAD DE SUCESO SIMULTANEO P(ai,bi): ",imprimirMatriz(prob_suceso_simul))
-    print("PROBABILIDAD CONDICIONAL P(ai/bi):", imprimirMatriz(prob_condicionales))
-    print("ENTROPIA APRIORI: H(A) = {}   H(B) = {}".format(entropia_Apriori_A, entropia_Apriori_B))
-    print("ENTROPIA APOSTERIORI: H(A/b=0) = {}   H(A/b=1)= {}".format(entropia_Aposteriori_0, entropia_Aposteriori_1))
+    print("MATRIZ DEL CANAL: \n", mat_canal)
+    print("PROBABILIDAD DE LA FUENTE DE ENTRADA P(ai): ", probF_E)
+    print("PROBABILIDAD DE LA FUENTE DE SALIDA P(bi): ", probF_S)
+    print("PROBABILIDAD DE SUCESO SIMULTANEO P(ai,bi): \n ", prob_suceso_simul),
+
+    print("PROBABILIDAD CONDICIONAL P(ai/bi):", prob_condicionales)
+    print(
+        "ENTROPIA APRIORI: H(A) = {}   H(B) = {}".format(
+            entropia_Apriori_A, entropia_Apriori_B
+        )
+    )
+    print(
+        "ENTROPIA APOSTERIORI: H(A/b=0) = {}   H(A/b=1)= {}".format(
+            entropia_Aposteriori_0, entropia_Aposteriori_1
+        )
+    )
     print("EQUIVOCACION: H(A/B) = ", equivocacion)
     print("INFORMACION MUTUA: I(A,B) = ", info_mutua)
 
@@ -171,12 +179,12 @@ def main():
     if action == "-p":
         Mat_Paridades = metodoParidadCruzada(mensajes, N, M)
         msnEnviado = enviaMensajes(Mat_Paridades, mat_canal)
-        print("MENSAJES A ENVIAR CON PARIDADES: ", imprimirMatriz(Mat_Paridades))
+        print("MENSAJES A ENVIAR CON PARIDADES: ", Mat_Paridades)
     else:
         msnEnviado = enviaMensajes(mensajes, mat_canal)
-        print("MENSAJES A ENVIAR: ", imprimirMatriz(mensajes))
+        print("MENSAJES A ENVIAR: ", mensajes)
 
-    print("MENSAJE ENVIADO:", imprimirMatriz(msnEnviado))
+    print("MENSAJE ENVIADO: ", msnEnviado)
 
 
 if __name__ == "__main__":
