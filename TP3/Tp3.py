55052595edc4763c056f86b0a8c2980e73d67f49
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-11-26 13:24:33 -0300

    d

--- TP3/Tp3.py
+++ TP3/Tp3.py
@@ -6,59 +6,59 @@ import math as ma
 
 
 def encode(data):
-    dictionary = {chr(i): i for i in range(256)}
-    current_code = 256
-    result = []
-    current_str = ""
+    diccionario = {chr(i): i for i in range(256)}
+    ActCode = 256
+    resultado = []
+    strAct = ""
     for char in data:
-        current_str += char
-        if current_str not in dictionary: 
-            result.append(dictionary[current_str[:-1]])
-            dictionary[current_str] = current_code
-            current_code += 1
-            current_str = char
+        strAct += char
+        if strAct not in diccionario: 
+            resultado.append(diccionario[strAct[:-1]])
+            diccionario[strAct] = ActCode
+            ActCode += 1
+            strAct = char
 
-    if current_str in dictionary:
-        result.append(dictionary[current_str])
+    if strAct in diccionario:
+        resultado.append(diccionario[strAct])
 
-    return result
+    return resultado
 
 
 def decode(data):
     # Descodifica un archivo de texto codificado con LZW.
 
-    dictionary = {i: chr(i) for i in range(256)}
-    current_code = 256
-    result = []
+    diccionario = {i: chr(i) for i in range(256)}
+    ActCode = 256
+    resultado = []
     previous_code = (data[1]) | (data[0]) << 8
-    entry = dictionary[previous_code]
-    result.append(dictionary[previous_code])  # inserta el primer caracter
+    entry = diccionario[previous_code]
+    resultado.append(diccionario[previous_code])  # inserta el primer caracter
 
     for i in range(2, len(data), 2):
         code = data[i + 1] | data[i] << 8
-        if code in dictionary:
-            current_entry = dictionary[code]
-        elif code == current_code:
+        if code in diccionario:
+            current_entry = diccionario[code]
+        elif code == ActCode:
             current_entry = entry + entry[0]
 
-        result.append(current_entry)
+        resultado.append(current_entry)
 
-        dictionary[current_code] = entry + current_entry[0]
-        current_code += 1
+        diccionario[ActCode] = entry + current_entry[0]
+        ActCode += 1
         entry = current_entry
 
-    return "".join(result)
+    return "".join(resultado)
 
 
-def metricas(original_file, compressed_file):
+def metricas(archOriginal, archCompress):
     df = pd.DataFrame(columns=["palabras", "apariciones", "probabilidad"])
-    df = frameDeDatos(compressed_file, df)
+    df = frameDeDatos(archCompress, df)
     E = calcDatos(df)
-    original_size = os.path.getsize("Samples/" + original_file)
-    compressed_size = os.path.getsize(compressed_file)
+    original_size = os.path.getsize("Samples/" + archOriginal)
+    compressed_size = os.path.getsize(archCompress)
     L = (
         compressed_size / original_size
-    ) * 8  # todo: Laclarar que es una estimacion pq no se pude hacer de la forma normal
+    ) * 8  
     efi = E / L
 
     return  1 - (compressed_size / original_size), efi
@@ -73,9 +73,9 @@ def calcDatos(df):
     return E
 
 
-def frameDeDatos(compressed_file, df):
+def frameDeDatos(archCompress, df):
     data = []
-    with open(compressed_file, "rb") as f:
+    with open(archCompress, "rb") as f:
         data = f.read().hex()
     data = bytes.fromhex(data)
     array_de_hex = [int(data[i + 1] | data[i] << 8) for i in range(0, len(data), 2)]
@@ -83,12 +83,12 @@ def frameDeDatos(compressed_file, df):
     diccionario = Counter(array_de_hex)
     tot = diccionario.total()
     for key in diccionario:
-        nwRow = {
+        col = {
             "palabras": key,
             "apariciones": diccionario[key],
             "probabilidad": diccionario[key] / tot,
         }
-        df = df._append(nwRow, ignore_index=True)
+        df = df._append(col, ignore_index=True)
     return df
 
 
@@ -99,32 +99,31 @@ def main():
         print("Uso: tpi3 original.txt compressed.bin {-c|-d}")
         sys.exit(1)
 
-    action = args[3]
-    original_file = args[1]
-    compressed_file = args[2]
+    opcion = args[3]
+    archOriginal = args[1]
+    archCompress = args[2]
 
-    if action == "-c":
-        with open("Samples/" + original_file, "r") as f:
+    if opcion == "-c":
+        with open("Samples/" + archOriginal, "r") as f:
             data = str(f.read())
         encoded = encode(data)
-        largo = len(encoded)
-        with open(compressed_file, "wb") as f:
+        with open(archCompress, "wb") as f:
             for i in encoded:
                 f.write(i.to_bytes(2, byteorder="big"))
 
         
-        compRatio, efi = metricas(original_file, compressed_file)
+        compRatio, efi = metricas(archOriginal, archCompress)
 
         print("Tasa de compresión: {:.2f}%".format(round(100 * compRatio, 2)))
         print("Eficiencia: {:.2f}%".format(round(100 * efi, 2)))
         print("Redundancia: {:.2f}%".format(round(100 * (1 - efi), 2)))
-    elif action == "-d":
-        with open(compressed_file, "rb") as f:
+    elif opcion == "-d":
+        with open(archCompress, "rb") as f:
             data = f.read().hex()
 
         data = bytes.fromhex(data)
         decoded = decode(data)
-        with open(original_file, "w", encoding="utf-8") as f:
+        with open(archOriginal, "w", encoding="utf-8") as f:
             f.write(decoded)
 
 
